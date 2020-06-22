2c6279d0755ab2d5d4ffa7b5481ac074b58bcb7d
Samuel Pomeroy <1690@holbertonschool.com>
2020-06-22 07:22:03 +0000

    updated to better handle NULL and indentical values

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,12 +9,13 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int i, total, max = array[0];
+	int i, total, max;
 	int *count, *output;
 
 	if (!array || size < 2)
 		return;
 
+	max = array[0];
 	for (i = 0; i < (int)size; i++)
 	{
 		if (array[i] > max)
@@ -24,7 +25,6 @@ void counting_sort(int *array, size_t size)
 	count = calloc((max + 1), sizeof(int));
 	if (!count)
 		return;
-
 	for (i = 0; i < (int)size; i++)
 		count[array[i]]++;
 
@@ -33,7 +33,6 @@ void counting_sort(int *array, size_t size)
 		total = count[i] + total;
 		count[i] = total;
 	}
-
 	print_array(count, max + 1);
 
 	output = malloc(sizeof(int) * size);
@@ -41,11 +40,13 @@ void counting_sort(int *array, size_t size)
 		return;
 
 	for (i = 0; i < (int)size; i++)
+	{
 		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--; /* needed to handle identical values */
+	}
 
 	for (i = 0; i < (int)size; i++)
 		array[i] = output[i];
-
 	free(count);
 	free(output);
 }
